f6b52b2e5c1781d11549697db55d97cfa8e51b16
Ajmal Jamal <ajipalara@gmail.com>
2018-05-22 12:09:49 +0530

    test_7

--- site/test/test.php
+++ site/test/test.php
@@ -1,1 +1,8 @@
-<?php echo `whoami`;
+<?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
